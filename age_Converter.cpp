90e4503f554d32a706332a3366f44f948c3a5fd7
Christian Schmidt <christian.schmidt@acad.ufsm.br>
2026-02-08 10:56:30 -0300

    base

--- age_Converter.cpp
+++ age_Converter.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int pegar_idade(){
+    int idade;
+    cout<<"Digite sua idade\n";
+    cin>>idade;
+    return idade;
+}
+void calc_idade(int idade){
+    int meses,semanas;
+    float horas, dias;
     
+    meses = idade * 12;
+    dias = idade * 365.25;
+    semanas = dias / 7;
+    horas = dias * 24;
+    
+    cout<<"voce tem: \n" << "Meses: "<< meses <<"\nSemanas: "<<semanas<< "\nDias: "<<dias<<"\nHoras: "<<horas;
+}
+int main(){
+    int idade = pegar_idade();
+    calc_idade(idade);
+    return 0;
 }
